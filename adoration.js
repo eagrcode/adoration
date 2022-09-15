7ea9a62a8e0499f577c8a59b31185c0bde816bfe
eagrcode <elliot.robinson92@hotmail.com>
2022-09-15 09:14:37 +0100

    first commit

---
+++ adoration.js
@@ -0,0 +1,13 @@
+function showHowMuchILoveYou(amount) {
+    if (typeof amount == 'number' && Number.isInteger(amount) && amount > 0) {
+        return `I love you th${'i'.repeat(amount)}s much!`
+    } else {
+        throw new Error("It's not you, it's me")
+    }
+}
+
+console.log(showHowMuchILoveYou(5));
+
+module.exports = {
+    showHowMuchILoveYou
+}
---
+++ adoration.test.js
@@ -0,0 +1,21 @@
+const { showHowMuchILoveYou } = require("./adoration");
+
+describe("showHowMuchILoveYou", () => {
+  it("Exists", () => {
+    expect(showHowMuchILoveYou).toBeDefined();
+  });
+
+  it("Is a function", () => {
+    expect(showHowMuchILoveYou instanceof Function).toEqual(true);
+  });
+
+  it("Returns a string with the correct number of 'i's", () => {
+    expect(showHowMuchILoveYou(5)).toBe("I love you thiiiiis much!");
+  });
+
+  it("Throws an error when passed a string", () => {
+    expect(() => {
+      showHowMuchILoveYou("with all my heart");
+    }).toThrow(new Error("It's not you, it's me"));
+  });
+});
