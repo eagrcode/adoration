b923c2e17f2dfacd1368049ee6177918438e4cf1
eagrcode <elliot.robinson92@hotmail.com>
2022-09-15 09:55:36 +0100

    oopsie

--- adoration.js
+++ adoration.js
@@ -1,13 +1,13 @@
 function showHowMuchILoveYou(amount) {
-    if (typeof amount == 'number' && Number.isInteger(amount) && amount > 0) {
-        return `I love you th${'i'.repeat(amount)}s much!`
-    } else {
-        throw new Error("It's not you, it's me")
-    }
+  if (typeof amount == "number" && Number.isInteger(amount) && amount > 0) {
+    return `I love you th${"7".repeat(amount)}s much!`;
+  } else {
+    throw new Error("It's not you, it's me");
+  }
 }
 
 console.log(showHowMuchILoveYou(5));
 
 module.exports = {
-    showHowMuchILoveYou
-}
+  showHowMuchILoveYou,
+};
